26e531fb5ffa986a24b5f91af5e051ea68fd47ee
ericb83 <eboerschinger@gmail.com>
2024-09-25 20:03:45 -0500

    updated navbar, footer, and hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,16 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="hero bg-base-200 min-h-screen ">
-      <div className="hero-content flex-col lg:flex-row">
+      <div className="hero-content flex-col lg:flex-row gap-20">
         <img
           src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-sm rounded-full shadow-2xl"
         />
-        <div className=" flex flex-col items-center">
-          <h2 className="text-3xl p-6 text-center">Hello!</h2>
+        <div className=" flex flex-col items-center gap-4">
           <h1 className="text-5xl font-bold px-6 text-center">
-            I'm Eric Boerschinger
+            Eric Boerschinger
           </h1>
           <h3 className="text-xl p-6 text-center"> Web Developer</h3>
-
-          <button className="btn btn-primary rounded-lg ">Get Started</button>
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,13 +26,13 @@ const NavBar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Homepage</a>
+              <a>About</a>
             </li>
             <li>
               <a>Projects</a>
             </li>
             <li>
-              <a>About</a>
+              <a>Education</a>
             </li>
             <li>
               <a>Contact</a>
