c4ec0ae9e0db80e67042fb0597590b1e160aa0b7
ericb83 <eboerschinger@gmail.com>
2024-10-13 19:21:47 -0500

    updated layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,6 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Eric Boerschinger",
@@ -22,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <NavBar />
         {children}
         <Footer />
