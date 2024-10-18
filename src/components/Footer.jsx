4b81f6db7034c83c0c89fcb388c64db1c71eeab7
ericb83 <eboerschinger@gmail.com>
2024-10-17 19:09:48 -0500

    update navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer bg-primary text-neutral-content p-4">
+    <footer className="footer bg-primary  p-4">
       <div className="container ">
-        <p className=" text-white text-lg ">
+        <p className=" text-black text-lg ">
           Copyright © {new Date().getFullYear()} - All rights reserved
         </p>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ export default function NavBar() {
     <div className="navbar bg-primary">
       <input
         type="checkbox"
+        aria-label="Theme"
         value="retro"
         className="  toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:theme(colors.sky.500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:theme(colors.blue.900)]"
       />
